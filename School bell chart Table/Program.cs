0069cc9252ca26c56edde16128731fffeed3c270
Ruper Ottolenghi <ruper120406@gmail.com>
2023-01-10 18:01:34 +0200

    Add project files.

---
+++ School bell chart Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_bell_chart_Table
+{
+    internal class Program
+    {
+        static void BoomBox()
+        {
+            SoundPlayer AngryBirds = new SoundPlayer(soundLocation: @"")
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
